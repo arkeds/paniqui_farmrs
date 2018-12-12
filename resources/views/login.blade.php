74d6169ca04480355a0c568c3b623a64eeaead20
Arvin de los Santos <adsantos.417@gmail.com>
2018-12-12 14:10:22 +0800

    Login form redesign

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -8,10 +8,11 @@
 <body>
 <style type="text/css">
 	body{
-		background-color: #808080;
+		background-color: #dadbd6;
 	}
+
 </style>
-<br><br><br>
+<br>
 <div class="container-fluid">
 	<div class="row">
 	<div class="col-md-3">
@@ -20,7 +21,11 @@
 	<div class="col-md-6">
 		<div class="card">
 			<div class="card-header bg-primary text-white">
-				Login to FarmRS
+				<center>
+					<img class="card-img-top" style="width: 250px; height: 250px;" src="{{asset("images/seal.png")}}">
+					<h3>Paniqui FarmRS</h3>
+					<p>Paniqui Farmers Registration System</p>
+				</center>
 			</div>
 			<div class="card-body">
 				@if(Session::has('status'))
@@ -30,16 +35,16 @@
 				@endif
 				<form action="{{url("/login")}}" method="POST">
 					<div class="form-group">
-						<label>Username</label>
-						<input required type="text" name="username" class="form-control" value="{{old('username')}}">
+						<input placeholder="Username" required type="text" name="username" class="form-control" value="{{old('username')}}">
 					</div>
 					<div class="form-group">
-						<label>Password</label>
-						<input required type="password" name="password" class="form-control" >
+						<input placeholder="Password" required type="password" name="password" class="form-control" >
 					</div>
-					<button type="submit" class="btn btn-primary">Login</button>
+					<button type="submit" class="btn btn-block btn-primary">Login</button>
 					@csrf
 				</form>
+				<br>
+			{{-- <p style="position: relative; text-align: center; bottom: -20px; margin-bottom: 0px;  bottom: 0px;">Developed By: PHilMech</p> --}}
 			</div>
 		</div>
 	</div>
